34b59417ac1949304d17623912b78bb636cb1d29
oveof <ovedy@oftedal.com>
2019-03-31 19:00:52 +0200

    Restructured and did some more minor stuff

---
+++ data.js
@@ -0,0 +1,55 @@
+var cabins = [
+    {
+        "name": "Granstua",
+        "beds": 4,
+        "standard": "Høy",
+        "sauna": 'Ja',
+        "price": 12000,
+        "images": 3,
+        "occupation": [
+
+        ]
+    },
+    {
+        "name": "Granbo",
+        "beds": 6,
+        "standard": "Middels",
+        "sauna": 'Nei',
+        "price": 15000,
+        "images": 3
+    },
+    {
+        "name": "Grantoppen",
+        "beds": 8,
+        "standard": "Lav",
+        "sauna": 'Nei',
+        "price": 16000,
+        "images": 3
+    },
+    {
+        "name": "Granhaug",
+        "beds": 10,
+        "standard": "Høy",
+        "sauna": 'Ja',
+        "price": 30000,
+        "images": 3
+    }
+];
+var occupation = [
+    {
+        "time": "Påske 2019",
+        "free": ["Granbo"]
+    },
+    {
+        "time": "Pinse 2019",
+        "free": ["Granstua", "Granbo", "Granhaug"]
+    },
+    {
+        "time": "Veke 28",
+        "free": ["Granstua", "Granbo", "Grantoppen", "Granhaug"]
+    },
+    {
+        "time": "Veke 29",
+        "free": ["Granbo", "Grantoppen"]
+    }
+];
